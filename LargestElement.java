9d1bcb4c7f81976609beebc64d48bd48d2066e6a
sangmesh-07 <sangmeshkanni820@gmail.com>
2026-02-25 18:29:55 +0530

    Largest Element in Array

---
+++ LargestElement.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+class LargestElement {
+
+    public static int largest(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();   // size of array
+        System.out.println("Enter array elements: ");
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(largest(arr));
+    }
+}
